26fb48e685bc5c261751ead4c514024972c75611
aboud20-meet <aboud20@meet.mit.edu>
2018-08-08 10:29:39 +0300

    f

--- exercises/app.py
+++ exercises/app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return render_template("index.html")
+	players=["sergio ramos" , "gareth bale" , "marcelo"]
+	return render_template("index.html", players=players , likes_same_sport=True)
 
 if __name__ == '__main__':
    app.run(debug = True)
